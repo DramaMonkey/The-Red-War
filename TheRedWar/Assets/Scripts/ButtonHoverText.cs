18e6d19c1a459e6fc8bb5110f5936cae2e8f3825
DramaMonkey <marcusclayton1997@gmail.com>
2020-11-06 16:04:37 +0000

    v1.4 - Checkpoints & Multiple levels
    Added a checkpoint system
    	sprites are basic but work for now
    
    Added a new level
    	Partially done, but needed multiple levels for...
    
    Added level select
    	Only 2 levels, but the concept works, can add more as needed
    
    Also fixed an issue with the interaction between checkpoints & swapping between levels

---
+++ TheRedWar/Assets/Scripts/ButtonHoverText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHoverText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	private Text myText;
+	public string NotHovering;
+	public string Hovering;
+				
+
+	void Start (){
+		myText = GetComponentInChildren<Text>();
+	}
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		myText.text = Hovering;
+	}
+
+	public void OnPointerExit (PointerEventData eventData)
+	{
+		myText.text = NotHovering;
+	}
+}
